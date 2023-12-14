48455523b145b5563461abc9b0287963174819a9
quizford <125616425+Astronaut828@users.noreply.github.com>
2023-12-14 00:08:49 +0000

    loading bar for commited message

--- packages/nextjs/components/example-ui/ContractData.tsx
+++ packages/nextjs/components/example-ui/ContractData.tsx
@@ -13,6 +13,7 @@ import {
 export const ContractData = () => {
   const { address } = useAccount();
   const [transitionEnabled, setTransitionEnabled] = useState(true);
+  const [currentPostIndex, setCurrentPostIndex] = useState(0);
 
   // CID Data
   const [userCIDs, setUserCIDs] = useState<string[]>([]);
@@ -27,9 +28,6 @@ export const ContractData = () => {
       timestamp?: string;
     };
   }
-
-  const url = `https://bafybeiby7vdd4deanl7x4glioxv4fehwabwjbls74g5dkvtgkm5fcrkw5a.ipfs.nftstorage.link/blob`;
-
 
   useScaffoldEventSubscriber({
     contractName: "YourContract",
@@ -103,6 +101,22 @@ export const ContractData = () => {
     }
   }, [userCIDs]);
 
+  // Function to show next post
+  const showNextPost = () => {
+    if (currentPostIndex < cidContents.length - 1) {
+      setCurrentPostIndex(currentPostIndex + 1);
+    }
+  };
+
+  // Function to show previous post
+  const showPreviousPost = () => {
+    if (currentPostIndex > 0) {
+      setCurrentPostIndex(currentPostIndex - 1);
+    }
+  };
+
+  // Get the current post
+  const currentPost = cidContents[currentPostIndex];
 
   // Use `myCidMappedEvents` to display a history of CID mappings for the user
   const {
@@ -116,75 +130,57 @@ export const ContractData = () => {
     filters: { user: address }, // Filter events for the current user
     blockData: true,
   });
-  
-  
 
   // console.log("Events:", isLoadingEvents, errorReadingEvents, myGreetingChangeEvents);
 
   console.log("yourContract: ", yourContract);
   const { showAnimation } = useAnimationConfig(cidCount);
 
-  return (
-<div className="flex flex-col justify-center items-center rounded-3xl bg-base-300 py-10 px-10 mt-5 lg:py-auto w-full max-w-[98vw]">
+return (
+    <div className="flex flex-col justify-center items-center rounded-3xl bg-base-300 py-10 px-10 mt-5 lg:py-auto w-full max-w-[98vw]">
+      <div className="flex flex-col items-center justify-center bg-base-100 rounded-3xl px-3 py-5 w-full" style={{ maxWidth: "95%" }}>
 
-      <div
-        className="flex flex-col items-center justify-center bg-base-100 rounded-3xl px-3 py-5 w-full"
-        style={{ maxWidth: "95%" }}
-      >
-
-
-        <div className="flex justify-between w-full px-20">
-
-          {/* Switch Button => turn to NEXT POST (Last posts) (Pagination) */}
-          <button
-            className="btn btn-circle btn-ghost relative bg-center bg-[url('/assets/switch-button-on.png')] bg-no-repeat"
-            onClick={() => {
-              setTransitionEnabled(!transitionEnabled);
-            }}
-          >
-            <div
-              className={`absolute inset-0 bg-center bg-no-repeat bg-[url('/assets/switch-button-off.png')] transition-opacity ${
-                transitionEnabled ? "opacity-0" : "opacity-100"
-              }`}
-            />
-          </button> 
- 
-
-
+        {/* Pagination Buttons and Post Count */}
+        <div className="flex justify-between w-full px-20 items-center">
+          {/* Pagination Buttons */}
+          <div className="flex gap-4">
+            <button className="btn" onClick={showPreviousPost} disabled={currentPostIndex === 0}>Previous</button>
+            <button className="btn" onClick={showNextPost} disabled={currentPostIndex === cidContents.length - 1}>Next</button>
+          </div>
+    
           {/* Post count */}
-          <div className="border border-primary rounded-xl flex">
-            <div className="p-2 py-1 border-r border-primary flex items-end">Post count</div>
-            <div className={`text-4xl text-right min-w-[3rem] px-2 py-1 flex justify-end font-bai-jamjuree ${showAnimation ? "animate-zoom" : ""}`}>
-              {cidCount?.toString() || "0"} {/* Display the CID count here */}
+          <div className="border border-primary rounded-xl flex justify-center items-center">
+            <div className="p-2 py-1 border-r border-primary">Post count</div>
+            <div className={`text-4xl px-2 py-1 font-bai-jamjuree ${showAnimation ? "animate-zoom" : ""}`}>
+              {cidCount?.toString() || "0"}
             </div>
           </div>
-
-
         </div>
 
-
         {/* CID list */}
-        <div className="flex flex-col justify-center items-center rounded-3xl bg-base-300 py-10 px-10 mt-5 lg:py-auto w-full max-w-[98vw]">
-          <div className="flex flex-col items-center justify-center bg-base-100 rounded-3xl px-3 py-5 w-full" style={{ maxWidth: "95%" }}>
+        <div className="flex flex-col justify-center items-center rounded-3xl bg-base-300 py-10 px-13 mt-5 lg:py-auto w-full max-w-[95vw]">
           
           <h3 className="text-lg font-bold mb-2">User CID's & Content:</h3>
-          <ul className="list-disc list-inside">
-              {address && cidContents.length > 0 ? (
-                cidContents.map(({ cid, content }, index) => (
-                  <li key={index}>
-                    CID: {cid}
-                    <br />
-                    Content: {content.text}
-                    <br />
-                    Timestamp: {content.timestamp ? new Date(content.timestamp).toLocaleString() : 'Unavailable'}
-                  </li>
-                ))
-              ) : (
-                <li>{address ? 'No CIDs found.' : 'Not logged in.'}</li>
-              )}
-            </ul>
+            {/* Display the current post */}
+            {address && cidContents.length > 0 ? (
+              <div className="mt-2 p-4">
+                {/* CID and Timestamp with emphasized labels */}
+                <p className="text-m mb-2">
+                  <strong>IPFS CID:</strong> {currentPost.cid} - 
+                  <strong> Timestamp:</strong> {currentPost.content.timestamp ? new Date(currentPost.content.timestamp).toLocaleString() : 'Unavailable'}
+                </p>
+
+                {/* Post Content */}
+                <div className="text-lg mt-5">
+                  <strong>Content:</strong>
+                  <p>{currentPost.content.text}</p>
+                </div>
+              </div>
+            ) : (
+              <p>{address ? 'No CIDs found.' : 'Not logged in.'}</p>
+            )}
             {/* ... other UI elements ... */}
-          </div>
+
         </div>
 
 
--- packages/nextjs/components/example-ui/ContractInteraction.tsx
+++ packages/nextjs/components/example-ui/ContractInteraction.tsx
@@ -7,74 +7,92 @@ const nftStorageApiKey = process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY;
 
 export const ContractInteraction = () => {
   const [visible, setVisible] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const [newText, saveNewText] = useState("");
   const maxChars = 1500;
 
   const { address } = useAccount();
 
-  const handleTextChange = (e) => {
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     if (e.target.value.length <= maxChars) {
       saveNewText(e.target.value);
     }
   };
 
-  const { writeAsync: writeCIDAsync, isLoading } = useScaffoldContractWrite({
+  const { writeAsync: writeCIDAsync } = useScaffoldContractWrite({
     contractName: "YourContract",
     functionName: "setUserCID",
     args: [undefined], // Provide an array with a single undefined element
-    onBlockConfirmation: txnReceipt => {
+    onBlockConfirmation: (txnReceipt) => {
       console.log("📦 Transaction blockHash", txnReceipt.blockHash);
       saveNewText("");
       console.log("CID set in contract:", txnReceipt.blockHash);
     },
   });
 
+  // Function to save the text as a JSON file and upload it to NFT.Storage
   const saveAsJsonFileAndSendToNFTStorage = async () => {
     if (!newText.trim()) {
       console.log("No text provided");
       return;
     }
 
+    setIsLoading(true);
+
     const timestamp = new Date().toISOString();
     const jsonData = {
       text: newText,
-      timestamp: timestamp
+      timestamp: timestamp,
     };
     const jsonContent = JSON.stringify(jsonData, null, 2);
-  
+
     try {
       const formData = new FormData();
-      formData.append('file', new Blob([jsonContent], { type: "application/json" }));
-  
-      const response = await fetch('https://api.nft.storage/upload', {
+      formData.append("file", new Blob([jsonContent], { type: "application/json" }));
+
+      const response = await fetch("https://api.nft.storage/upload", {
         method: "POST",
         headers: {
-          "Authorization": `Bearer ${nftStorageApiKey}`
+          Authorization: `Bearer ${nftStorageApiKey}`,
         },
         body: formData,
       });
-  
+
       if (response.ok) {
         const data = await response.json();
         const cid = data.value.cid;
         console.log(`Uploaded to NFT.Storage - CID: ${cid}`);
-  
+
         // Call writeCIDAsync with the CID when it's available
         try {
           const txnReceipt = await writeCIDAsync({ args: [cid] });
+          setIsLoading(false); // Reset isLoading once the operation is complete
         } catch (writeError) {
           console.error("Failed to write CID to the contract:", writeError);
+          setIsLoading(false); // Reset isLoading in case of an error
         }
       } else {
         console.error("Failed to upload to NFT.Storage");
         console.log("Status:", response.status);
         console.log("Response:", await response.text());
+        setIsLoading(false); // Reset isLoading in case of an error
       }
     } catch (uploadError) {
       console.error("An error occurred while uploading to NFT.Storage", uploadError);
+      setIsLoading(false); // Reset isLoading in case of an error
     }
   };
+
+  // Loading bar animation
+  const LoadingBar = () => {
+    return (
+      <div className="w-44 p-0.5 flex items-center bg-neutral border border-primary rounded-full">
+        <div className="h-1.5 border border-primary rounded-full bg-secondary animate-grow"></div>
+      </div>
+    );
+  };
+
   
   
   return (
@@ -106,11 +124,13 @@ export const ContractInteraction = () => {
             <button
               className="btn btn-primary rounded-full capitalize font-normal font-white w-30 flex items-center gap-1 hover:gap-2 transition-all tracking-widest"
               onClick={saveAsJsonFileAndSendToNFTStorage}
-              disabled={isLoading || !address} // Disable the button if loading or no user is logged in
+              disabled={isLoading || !address}
             >
               {isLoading ? (
-                <span className="loading loading-spinner loading-sm"></span>
+                // Display the Loading Bar when isLoading is true
+                <LoadingBar />
               ) : (
+                // Display text based on address state
                 address ? "Commit Your Msg" : "Connect Wallet"
               )}
             </button>
